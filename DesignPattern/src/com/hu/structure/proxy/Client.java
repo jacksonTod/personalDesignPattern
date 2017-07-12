4488a9b23cbb7065856248c6243f669dcbf876fa
test_hu <107187929@qq.com>
2017-07-12 14:14:32 +0800

    this is test

--- DesignPattern/src/com/hu/structure/proxy/Client.java
+++ DesignPattern/src/com/hu/structure/proxy/Client.java
@@ -3,7 +3,6 @@ package com.hu.structure.proxy;
 public class Client {
 
 	public static void main(String[] args) {
-        // TODO Auto-generated method stub
         AbstractObject obj = new ProxyObject();
         obj.operation();
     }
